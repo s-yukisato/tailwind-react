452b8ebb8310e6fa7955666faee1e27f5f27bf7e
yuki <s1932058jl@s.chibakoudai.jp>
2021-07-29 22:46:04 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        sky: colors.sky,
+        cyan: colors.cyan,
+        violet : colors.violet
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
